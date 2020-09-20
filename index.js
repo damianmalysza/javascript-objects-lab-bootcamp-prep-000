c1ca98a423adb445d108d2e3f9456574cf130ea3
Damian Malysza <damian.malysza@gmail.com>
2020-09-20 20:38:20 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,5 +8,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
 }
 
 function deleteFromObjectByKey(object, key){
-  
+  var newObj=Object.assign({},object)
+  newObj.delete[key]
+  return newObj
 }
